d212fb1a7fba8b1f2426d9f33ec240c5cbc24e65
Shivani6668 <shivaniparmar316@gmail.com>
2023-11-30 14:01:09 +0530

    Complete

---
+++ constant.js
@@ -0,0 +1,6 @@
+export const data = [
+    "https://th.bing.com/th/id/R.c969f1d0a12e2ac2d5c44cde7bd9fd36?rik=JL%2bxtDA09B6hWA&riu=http%3a%2f%2f4.bp.blogspot.com%2f-Me3-M0U9XIU%2fTn3Wr7YJDsI%2fAAAAAAAAAO0%2fEcrlIFlGEwM%2fs1600%2ffamily%2bdogs-473268.jpeg&ehk=IaPlXbN%2fYSBJIAZ1LB74T%2brf6KU4xmZDnQXS%2bMOwAD0%3d&risl=&pid=ImgRaw&r=0",
+    "https://th.bing.com/th/id/OIP.69cownMYGCgV1vYYqva4DwHaE6?w=650&h=431&rs=1&pid=ImgDetMain",
+    "https://environment.des.qld.gov.au/__data/assets/image/0027/86661/dingo_lge.jpg",
+    "https://th.bing.com/th/id/R.99935518ad38658c67b672a30b36aba5?rik=NgHCyieLtBos7g&riu=http%3a%2f%2fpower-pet.com%2fwordpress%2fwp-content%2fuploads%2f2013%2f06%2fcute-golden-retriever-puppy-animals-baby-animals-dogs-golden.jpg&ehk=Fk2FhPckK9KXN070wcaRo9aEmX17SwIFA40brmZ27FI%3d&risl=&pid=ImgRaw&r=0",
+  ];
--- src/ImageSlider.jsx
+++ src/ImageSlider.jsx
@@ -1,10 +1,79 @@
-const ImageSlider = () =>{
-    return (
-        <>
-      <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-        </>
-    )
-}
-export default ImageSlider
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import { useState } from "react";
+import { data } from "../constant";
+
+const ImageSlider = () => {
+  const [activeImage, setActiveImage] = useState(0);
+
+  const handlenext = () => {
+    return setActiveImage((activeImage + 1) % data.length);
+  };
+
+  const handleprevious = () => {
+    return setActiveImage(!activeImage ? data.length - 1 : activeImage - 1);
+  };
+
+  useEffect(() => {
+    const Timer = setTimeout(() => {
+      handlenext();
+    }, 3000);
+    return () => {
+      clearTimeout(Timer);
+    };
+  }, [activeImage]);
+
+  return (
+    <>
+      <h1 className="mb-4 text-center text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">
+        Image Slider
+      </h1>
+
+      <div className="flex justify-center">
+        <button onClick={handleprevious}>
+          <svg
+            class="h-16 w-16 text-red-500"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z"
+            />
+          </svg>
+        </button>
+
+        {data.map((url, i) => (
+          <img
+            className={
+              "h-[500px] w-[700px] rounded-lg object-contain " +
+              (activeImage === i ? "block" : "hidden")
+            }
+            src={url}
+            alt="image description"
+          />
+        ))}
+
+        <button onClick={handlenext}>
+          <svg
+            class="h-16 w-16 text-red-500"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        </button>
+      </div>
+    </>
+  );
+};
+export default ImageSlider;
